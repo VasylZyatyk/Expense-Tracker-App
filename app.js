57ee591b3346ed806c08a53f8cf6d562c3570330
Yurii <135170315+YuriiZiatyk@users.noreply.github.com>
2023-06-28 12:47:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const fs = require('fs')
-require('dot.env').config
-const PORT = process.env.PORT || process
-// middleware '
-app.use(express.json)
-app.use(cors())
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const fs = require('fs');
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
 // routes
-fs('./routes').map(route)=>
-app.use('/api/v1',require'./routes/'+routes)
-app.get('/',(req, res,) =>{
-    res.send('Hello World')
-})
+fs.readdirSync('./routes').map(route => {
+    app.use('/api/v1', require('./routes/' + route));
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
 const server = () => {
-    db()
-    app.use(cors())
-        app.use(express.json())
-        app.use(express.urlencoded({ extended: true }))
-        app.use(require('./routes'))
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`)
-        })
-}
-server()
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+};
+
+server();
